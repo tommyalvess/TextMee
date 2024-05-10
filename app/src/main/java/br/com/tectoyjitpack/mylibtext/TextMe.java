0485207317b0e37a0c864ed774bb23a44f76a149
eliton.santos@tectoy.com.br <eliton.santos@tectoy.com>
2024-05-10 10:47:37 -0300

    first commit

---
+++ app/src/main/java/br/com/tectoyjitpack/mylibtext/TextMe.java
@@ -0,0 +1,16 @@
+package br.com.tectoyjitpack.mylibtext;
+
+import android.content.Context;
+import android.os.Build;
+
+public class TextMe {
+    Context context;
+
+    public TextMe(Context context) {
+        this.context = context;
+    }
+
+    public String getManufacturer(){
+        return Build.MANUFACTURER.toString();
+    }
+}
